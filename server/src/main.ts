1f1b9cf7d71bffb02f7a6000e1507373d29140b2
oasis345 <cogudrnr9@naver.com>
2023-06-20 18:53:34 +0900

    request limit 한도 수정.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ErrorFilter } from 'error.filter';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   app.useGlobalFilters(new ErrorFilter());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();
